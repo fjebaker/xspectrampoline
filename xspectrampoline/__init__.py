a6d3693f442bd952d258f86d062fd2abfc058205
fjebaker <fergus@cosroe.com>
2026-02-28 12:05:13 +0000

    feat: error-sensitive initialisation
    Print some hopefully informative errors if LibXSPEC cannot be loaded.

--- xspectrampoline/__init__.py
+++ xspectrampoline/__init__.py
@@ -1,12 +1,50 @@
-import importlib
+import importlib.resources
 import ctypes
+import os
+import pathlib
+import traceback
+import logging
 
-__xspec_path = importlib.resources.files("xspectrampoline") / "LibXSPEC_v6_35_1"
-os.environ["HEADAS"] = str(__xspec_path.abspath())
 
-lib_XS = ctypes.CDLL(__xspec_path / "lib" / "libXS.so")
-lib_XSFunctions = ctypes.CDLL(__xspec_path / "lib" / "libXSFunctions.so")
-lib_XSUtil = ctypes.CDLL(__xspec_path / "lib" / "libXSUtil.so")
+class NoLibXSPEC(Exception): ...
+
+
+logger = logging.getLogger(__name__)
+
+__xspectrampoline_path = (
+    importlib.resources.files("xspectrampoline") / "LibXSPEC_v6_35_1"
+)
+
+if "HEADAS" not in os.environ:
+    os.environ["HEADAS"] = str(__xspectrampoline_path)
+
+__headas_path = pathlib.Path(os.environ.get("HEADAS"))
+
+# Load the various libraries
+try:
+    lib_XS = ctypes.CDLL(__headas_path / "lib" / "libXS.so")
+    lib_XSFunctions = ctypes.CDLL(__headas_path / "lib" / "libXSFunctions.so")
+    lib_XSUtil = ctypes.CDLL(__headas_path / "lib" / "libXSUtil.so")
+except OSError:
+    logging.error(traceback.format_exc())
+    logging.error(
+        "Failed to load LibXSPEC. If you have $HEADAS set in your envrionment, "
+        "try unsetting it to use the bundled XSPEC installation. If HEADAS is "
+        "not set, please open an issue on the xspectrampoline repository with "
+        "the logs and a brief description of your environment."
+    )
+    raise NoLibXSPEC()
+
+
+# Initialise XSPEC functions
 lib_XSFunctions.FNINIT()
 
-print("INIT DONE")
+
+def getHEADAS() -> str:
+    """
+    Returns the HEADAS path currently used by the library.
+    """
+    return str(__headas_path)
+
+
+__all__ = [getHEADAS]
