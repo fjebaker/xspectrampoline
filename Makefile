12fbac3a32b42d79cd0ecbafe5899fc1897f286f
fjebaker <fergus@cosroe.com>
2026-02-28 14:26:20 +0000

    feat: bundle CompilerSupportLibraries
    This should avoid having to rely on system libraries being present,
    resolvingn vor gfortran and so on.

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 
 LIBXSPEC_VERSION = v6.35.1
 LIBXSPEC_BUILD_VERSION = +1
+COMPILERSUPPORT_VERSION = v1.3.1
+COMPILERSUPPORT_BUILD_VERSION = +0
 
 ARTIFACTS_DIR := artifacts
 
@@ -8,25 +10,35 @@ ALL_PLATFORMS := x86_64-linux-gnu-libgfortran5 \
 				 aarch64-apple-darwin-libgfortran5
 
 ALL_LIBXSPEC_PATHS := $(foreach PLAT,$(ALL_PLATFORMS),LibXSPEC.$(LIBXSPEC_VERSION).$(PLAT))
-ALL_ARTIFACT_PATHS := $(foreach ARTIFACT,$(ALL_LIBXSPEC_PATHS),$(ARTIFACTS_DIR)/$(ARTIFACT))
+ALL_SUPPORT_PATHS := $(foreach PLAT,$(ALL_PLATFORMS),CompilerSupportLibraries.$(COMPILERSUPPORT_VERSION).$(PLAT))
 
-ARTIFACT_DOWNLOAD_ROOT := \
+ALL_LIBXSPEC_ARTIFACTS := $(foreach ARTIFACT,$(ALL_LIBXSPEC_PATHS),$(ARTIFACTS_DIR)/$(ARTIFACT))
+ALL_SUPPORT_ARTIFACTS := $(foreach ARTIFACT,$(ALL_SUPPORT_PATHS),$(ARTIFACTS_DIR)/$(ARTIFACT))
+
+LIBXSPEC_DOWNLOAD_ROOT := \
 	https://github.com/astro-group-bristol/LibXSPEC_jll.jl/releases/download/LibXSPEC-$(LIBXSPEC_VERSION)$(LIBXSPEC_BUILD_VERSION)
+SUPPORT_DOWNLOAD_ROOT := \
+	https://github.com/JuliaBinaryWrappers/CompilerSupportLibraries_jll.jl/releases/download/CompilerSupportLibraries-$(COMPILERSUPPORT_VERSION)$(COMPILERSUPPORT_BUILD_VERSION)
 
 all: all-artifacts wheels
 
 .PHONY: all-artifacts
-all-artifacts: $(ARTIFACTS_DIR) $(ALL_ARTIFACT_PATHS)
+all-artifacts: $(ARTIFACTS_DIR) $(ALL_LIBXSPEC_ARTIFACTS) $(ALL_SUPPORT_ARTIFACTS)
 
 $(ARTIFACTS_DIR):
 	mkdir -p $@
 
-# Download the artifact from the remote location
-$(ARTIFACTS_DIR)/%.tar.gz:
-	curl -SL $(ARTIFACT_DOWNLOAD_ROOT)/$*.tar.gz \
+# Download the LibXSPEC artifact from the remote location
+$(ARTIFACTS_DIR)/LibXSPEC%.tar.gz:
+	curl -SL $(LIBXSPEC_DOWNLOAD_ROOT)/LibXSPEC$*.tar.gz \
 		-o $@
 
-# Unpack the artifact
+# Download the CompilerSupportLibraries artifact from the remote location
+$(ARTIFACTS_DIR)/CompilerSupportLibraries%.tar.gz:
+	curl -SL $(SUPPORT_DOWNLOAD_ROOT)/CompilerSupportLibraries$*.tar.gz \
+		-o $@
+
+# Unpack the artifacts
 $(ARTIFACTS_DIR)/%: $(ARTIFACTS_DIR)/%.tar.gz
 	mkdir -p $@
 	tar -C $@ -xzf $<
--- dist-package.py
+++ dist-package.py
@@ -9,6 +9,7 @@ import base64
 import traceback
 
 LIBXSPEC_VERSION = "v6.35.1"
+COMPILER_SUPPORT_VERSION = "v1.3.1"
 
 
 def make_record_entry(filepath: str, package_root: str = "", name="") -> str:
@@ -40,7 +41,11 @@ def zipdir(path: str, zf: zipfile.ZipFile):
 
 
 def repackage(
-    root: str, filename: str, libxspec: str, platform_tag: str = "py3-none-linux_x86_64"
+    root: str,
+    filename: str,
+    libxspec: str,
+    support: str,
+    platform_tag: str = "py3-none-linux_x86_64",
 ):
     # TODO: get the version string from the filename
     version = "0.1.0"
@@ -56,6 +61,13 @@ def repackage(
     xspec_dir_path = os.path.join(root, xspec_dir)
 
     shutil.copytree(os.path.join("artifacts", libxspec), xspec_dir_path)
+    # Copy the CompilerSupportLibraries and merge lib and share
+    for directory in ("lib", "share"):
+        shutil.copytree(
+            os.path.join("artifacts", support, directory),
+            os.path.join(xspec_dir_path, directory),
+            dirs_exist_ok=True,
+        )
 
     record_entries = []
     for dirpath, dirnames, filenames in os.walk(xspec_dir_path):
@@ -122,23 +134,26 @@ if __name__ == "__main__":
         (
             "py3-none-linux_x86_64",
             f"LibXSPEC.{LIBXSPEC_VERSION}.x86_64-linux-gnu-libgfortran5",
+            f"CompilerSupportLibraries.{COMPILER_SUPPORT_VERSION}.x86_64-linux-gnu-libgfortran5",
         ),
         (
             "py3-none-macosx_15_0_arm64",
             f"LibXSPEC.{LIBXSPEC_VERSION}.aarch64-apple-darwin-libgfortran5",
+            f"CompilerSupportLibraries.{COMPILER_SUPPORT_VERSION}.aarch64-apple-darwin-libgfortran5",
         ),
     ]
     original_wheel = "./dist/xspectrampoline-0.1.0-py3-none-any.whl"
 
     no_errors = True
 
-    for platform_tag, libxspec in platform_info:
+    for platform_tag, libxspec, support in platform_info:
         print(f"Packaging for {platform_tag}")
         try:
             repackage(
                 os.path.abspath("delete-me"),
                 original_wheel,
                 libxspec,
+                support,
                 platform_tag=platform_tag,
             )
         except Exception as e:
