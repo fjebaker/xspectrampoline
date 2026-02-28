185bfd15844d1d7cc2903f04f1c6c2dafb96babb
fjebaker <fergus@cosroe.com>
2026-02-28 12:04:51 +0000

    feat: dist-package for different platforms

--- dist-package.py
+++ dist-package.py
@@ -6,28 +6,37 @@ import pathlib
 import time
 import zipfile
 import base64
+import traceback
 
-def make_record_entry(filepath: str, package_root: str="", name="") -> str:
+LIBXSPEC_VERSION = "v6.35.1"
+
+
+def make_record_entry(filepath: str, package_root: str = "", name="") -> str:
     with open(filepath, "rb") as f:
         digest = hashlib.file_digest(f, "sha256")
 
     digest_bytes = digest.digest()
 
     filesize = os.path.getsize(filepath)
-    relative_filename = name or filepath[filepath.find(package_root):]
+    relative_filename = name or filepath[filepath.find(package_root) :]
 
-    base64_digest = base64.urlsafe_b64encode(digest_bytes).rstrip(b'=').decode()
+    base64_digest = base64.urlsafe_b64encode(digest_bytes).rstrip(b"=").decode()
 
     return f"{relative_filename},sha256={base64_digest},{filesize}"
+
 
 def zipdir(path: str, zf: zipfile.ZipFile):
     for root, dirs, files in os.walk(path):
         for file in files:
-            zf.write(os.path.join(root, file),
-                       os.path.relpath(os.path.join(root, file),
-                                       os.path.join(path)))
+            zf.write(
+                os.path.join(root, file),
+                os.path.relpath(os.path.join(root, file), os.path.join(path)),
+            )
 
-def repackage(root: str, filename: str, platform_tag: str = "py3-none-linux_x86_64"):
+
+def repackage(
+    root: str, filename: str, libxspec: str, platform_tag: str = "py3-none-linux_x86_64"
+):
     # TODO: get the version string from the filename
     version = "0.1.0"
 
@@ -41,27 +50,31 @@ def repackage(root: str, filename: str, platform_tag: str = "py3-none-linux_x86_
     # The actual path on the machine to where the files currently are
     xspec_dir_path = os.path.join(root, xspec_dir)
 
-    shutil.copytree("./artifacts/LibXSPEC.v6.35.1.x86_64-linux-gnu-libgfortran5", xspec_dir_path)
+    shutil.copytree(os.path.join("artifacts", libxspec), xspec_dir_path)
 
     record_entries = []
-    for (dirpath, dirnames, filenames) in os.walk(xspec_dir_path):
+    for dirpath, dirnames, filenames in os.walk(xspec_dir_path):
         filepaths = [os.path.join(dirpath, f) for f in filenames]
 
-        record_entries.extend((make_record_entry(f, package_root=xspec_dir) for f in  filepaths))
+        record_entries.extend(
+            (make_record_entry(f, package_root=xspec_dir) for f in filepaths)
+        )
 
     # Rewrite the tag for the particular place we're packaging
     dist_info_name = f"xspectrampoline-{version}.dist-info"
     dist_info_path = pathlib.Path(root) / dist_info_name
 
     wheel_path = dist_info_path / "WHEEL"
-    new_wheel = wheel_path.read_text().replace("Tag: py3-none-any", f"Tag: {platform_tag}")
+    new_wheel = wheel_path.read_text().replace(
+        "Tag: py3-none-any", f"Tag: {platform_tag}"
+    )
     wheel_path.write_text(new_wheel)
 
     # Recalculate the WHEEL record entry
     wheel_record = make_record_entry(str(wheel_path), name=f"{dist_info_name}/WHEEL")
 
     # Write the new record file
-    record_file =  dist_info_path / "RECORD"
+    record_file = dist_info_path / "RECORD"
 
     new_record = record_entries + record_file.read_text().splitlines()
     new_record = [r for r in new_record if "info/WHEEL,sha" not in r] + [wheel_record]
@@ -75,7 +88,6 @@ def repackage(root: str, filename: str, platform_tag: str = "py3-none-linux_x86_
 
     # TODO: copy over the licenses
 
-
     # Move everything into a directory
     new_name = f"xspectrampoline-{version}-{platform_tag}"
     new_dir = os.path.join(root, new_name)
@@ -86,7 +98,7 @@ def repackage(root: str, filename: str, platform_tag: str = "py3-none-linux_x86_
     # Now rezip and pray. Need to use zipfile here because of the timestamps of
     # the files
     new_wheel = f"{new_name}.whl"
-    with zipfile.ZipFile(new_wheel, 'w', strict_timestamps=False) as zf:
+    with zipfile.ZipFile(new_wheel, "w", strict_timestamps=False) as zf:
         zipdir(new_dir + "/", zf)
 
     # And restore it's location
@@ -100,4 +112,38 @@ if __name__ == "__main__":
     except:
         ...
     os.mkdir(tmpdir)
-    repackage(os.path.abspath("delete-me"), "./dist/xspectrampoline-0.1.0-py3-none-any.whl")
+
+    platform_info = [
+        (
+            "py3-none-linux_x86_64",
+            f"LibXSPEC.{LIBXSPEC_VERSION}.x86_64-linux-gnu-libgfortran5",
+        ),
+        (
+            "py3-none-macos_aarch64",
+            f"LibXSPEC.{LIBXSPEC_VERSION}.aarch64-apple-darwin-libgfortran5",
+        ),
+    ]
+    original_wheel = "./dist/xspectrampoline-0.1.0-py3-none-any.whl"
+
+    no_errors = True
+
+    for platform_tag, libxspec in platform_info:
+        print(f"Packaging for {platform_tag}")
+        try:
+            repackage(
+                os.path.abspath("delete-me"),
+                original_wheel,
+                libxspec,
+                platform_tag=platform_tag,
+            )
+        except Exception as e:
+            print(traceback.format_exc())
+            no_errors = False
+
+    shutil.rmtree(tmpdir)
+
+    if no_errors:
+        print(f"No errors occured. Deleting the original wheel '{original_wheel}'")
+        os.remove(original_wheel)
+    else:
+        print("Errors occured.")
