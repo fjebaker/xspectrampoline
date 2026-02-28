30a36f49fb01f6077a3bc94bcf1de70b2c9de3ec
fjebaker <fergus@cosroe.com>
2026-02-28 16:23:05 +0000

    chore: add numpy as install dependency

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     author_email="fergus@cosroe.com",
     description="A package for distributing the XSPEC model library.",
     python_requires=">=3.6",
+    install_requires=["numpy>=1.24"],
     license="GPL-3.0-or-later",
     name=package_name,
     version=version,
