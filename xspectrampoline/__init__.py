d52d170755b086d2cfd1d7b3ae7eeb43a5e5f8dd
fjebaker <fergus@cosroe.com>
2026-02-28 16:20:00 +0000

    feat: minimal model wrappers

--- xspectrampoline/__init__.py
+++ xspectrampoline/__init__.py
@@ -5,6 +5,9 @@ import pathlib
 import traceback
 import logging
 import sys
+import numpy
+
+from typing import Optional
 
 SHARED_LIB_EXT = ".so" if sys.platform.startswith("linux") else ".dylib"
 
@@ -25,7 +28,94 @@ __headas_path = pathlib.Path(os.environ.get("HEADAS"))
 
 
 def _dlopen_wrapper(path: pathlib.Path) -> ctypes.CDLL:
+    """
+    A wrapper around `ctypes.CDLL` that is used for all `dlopen` calls. The
+    indended purpose is to configure all the mode flags in one place.
+    """
     return ctypes.CDLL(path, mode=ctypes.RTLD_GLOBAL)
+
+
+class LibXSPEC:
+    def __init__(self, lib_xs, lib_xs_functions, lib_xs_utils):
+        self.lib_xs = lib_XS
+        self.lib_xs_functions = lib_xs_functions
+        self.lib_xs_utils = lib_xs_utils
+
+    @staticmethod
+    def _wrap_Fortran_interface(f) -> callable:
+        f.argtypes = [
+            ctypes.POINTER(ctypes.c_float),
+            ctypes.POINTER(ctypes.c_int),
+            ctypes.POINTER(ctypes.c_float),
+            ctypes.POINTER(ctypes.c_int),
+            ctypes.POINTER(ctypes.c_float),
+            ctypes.POINTER(ctypes.c_float),
+        ]
+        f.restype = None
+
+        def _wrapper(
+            domain: np.array, parameter: np.array, output: np.array, error: np.array
+        ) -> None:
+            return f(
+                domain.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+                ctypes.byref(ctypes.c_int(len(output))),
+                parameter.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+                ctypes.byref(ctypes.c_int(1)),
+                output.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+                error.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+            )
+
+        return _wrapper
+
+    @staticmethod
+    def _wrap_C_interface(f) -> callable:
+        f.argtypes = [
+            ctypes.POINTER(ctypes.c_double),
+            ctypes.c_int,
+            ctypes.POINTER(ctypes.c_double),
+            ctypes.c_int,
+            ctypes.POINTER(ctypes.c_double),
+            ctypes.POINTER(ctypes.c_double),
+            ctypes.c_char_p,
+        ]
+        f.restype = None
+
+        def _wrapper(
+            domain: np.array,
+            parameter: np.array,
+            output: np.array,
+            error: np.array,
+            init_str: str = "",
+        ) -> None:
+            return f(
+                domain.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+                ctypes.c_int(len(output)),
+                parameter.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+                ctypes.c_int(1),
+                output.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+                error.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+                init_str.encode("ascii"),
+            )
+
+        return _wrapper
+
+    def get_model(self, symbol: str, interface: Optional[str] = None) -> callable:
+        """
+        Obtain a callable that invokes a model with the standard XSPEC model
+        interface.
+        """
+        f = getattr(self.lib_xs_functions, symbol)
+        if interface == "fortan" or symbol.endswith("_"):
+            return self._wrap_Fortran_interface(f)
+        elif interface == "c" or symbol.startswith("C_"):
+            return self._wrap_C_interface(f)
+        else:
+            if interface is not None:
+                raise Exception(f"Unknown function interface '{interface}'")
+            else:
+                raise Exception(
+                    f"Could not determine interface for '{symbol}'. Use the `interface` kwarg to set the wrapper type."
+                )
 
 
 # Load the various libraries
@@ -48,6 +138,13 @@ except OSError:
 
 # Initialise XSPEC functions
 lib_XSFunctions.FNINIT()
+
+
+def get_libXSPEC() -> LibXSPEC:
+    """
+    Get a handle to all of the XSPEC libraries.
+    """
+    return LibXSPEC(lib_XS, lib_XSFunctions, lib_XSUtil)
 
 
 def getHEADAS() -> str:
