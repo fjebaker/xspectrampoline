c291d6a8201ffb34e6204fef389cec8ff0d92e8d
fjebaker <fergus@cosroe.com>
2026-02-28 16:20:14 +0000

    test: add simple powerlaw test

---
+++ test/test_basics.py
@@ -0,0 +1,17 @@
+import pytest
+import xspectrampoline
+import numpy as np
+
+
+def test_powerlaw():
+    lib = xspectrampoline.get_libXSPEC()
+    powerlaw = lib.get_model("C_powerlaw")
+
+    energy = np.linspace(0.01, 10.0, 100, dtype=np.float64)
+    parameters = np.array([3.0], dtype=np.float64)
+    output = np.zeros(len(energy) - 1, dtype=np.float64)
+    error = np.zeros(len(energy) - 1, dtype=np.float64)
+
+    powerlaw(energy, parameters, output, error)
+
+    assert pytest.approx(output.sum(), 1e-1) == 5000.0
